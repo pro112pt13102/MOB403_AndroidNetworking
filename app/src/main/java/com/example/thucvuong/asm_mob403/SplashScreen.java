e86e47e8404788755598aa23d9f166cb8d21f5ee
pro112.pt13102 <pro112.pt13102@gmail.com>
2018-11-17 02:24:52 +0700

    Get Data from Restful API success

--- app/src/main/java/com/example/thucvuong/asm_mob403/SplashScreen.java
+++ app/src/main/java/com/example/thucvuong/asm_mob403/SplashScreen.java
@@ -1,9 +1,21 @@
 package com.example.thucvuong.asm_mob403;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sutrix.common.Common;
+import com.sutrix.common.HTTPDataHandler;
+import com.sutrix.model.Truyen;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -13,10 +25,17 @@ public class SplashScreen extends AppCompatActivity{
     private int i=0;
     TextView textView;
 
+    //DucNguyen ArrayList
+    ArrayList<Truyen> truyens = new ArrayList<Truyen>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //DucNguyen
+        new GetData().execute(Common.getAddressAPI());
+
         progressBar=(ProgressBar)findViewById(R.id.progressBar);
         progressBar.setProgress(0);
         textView=(TextView)findViewById(R.id.textView);
@@ -47,6 +66,75 @@ public class SplashScreen extends AppCompatActivity{
                 }
             }
         }, 0, period);
+    }
+
+    //DucNguyen getData
+    class GetData extends AsyncTask<String, Void, String> {
+
+        ProgressDialog pd = new ProgressDialog(SplashScreen.this);
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pd.setTitle("Please Wait ...");
+            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String temp = null;
+
+            String urlString = strings[0];
+
+            HTTPDataHandler http = new HTTPDataHandler();
+
+            temp = http.getHTTPData(urlString);
+
+            return temp;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            JSONArray jsonArray = null;
+
+            Object temp = null;
+            String $oid = null;
+
+            try {
+
+                jsonArray = new JSONArray(s);
+
+                for (int i=0; i<jsonArray.length(); i++){
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    Truyen truyen = new Truyen();
+
+                    JSONObject _id = jsonObject.getJSONObject("_id");
+                    $oid = _id.getString("$oid");
+
+//                    truyen.setTieuDe(jsonObject.getString("TieuDe"));
+//                    truyen.setNoiDung(jsonObject.getString("NoiDung"));
+//                    truyen.setMoTa(jsonObject.getString("MoTa"));
+
+                    truyens.add(truyen);
+
+                }
+
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+
+//            Toast.makeText(SplashScreen.this, $oid+"", Toast.LENGTH_SHORT).show();
+
+            pd.dismiss();
+        }
     }
 
 }
